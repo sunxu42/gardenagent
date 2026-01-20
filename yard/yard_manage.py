189385aa96a9adc1c64a192d8e4b418ca1243a55
lpl <linpeilu@fairlandgroup.com>
2026-01-20 18:13:13 +0800

    update

--- yard/yard_manage.py
+++ yard/yard_manage.py
@@ -1,12 +1,22 @@
 import os
-import asyncio
 from dotenv import load_dotenv
-load_dotenv() 
+load_dotenv("/home/diska/ongoing/gardenAgent/yard/.env") 
+import asyncio
+import yaml
 from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from deepagents import create_deep_agent
+from deepagents.backends import FilesystemBackend
+from langgraph.checkpoint.memory import MemorySaver
 
 GLM_API_KEY = os.getenv('GLM_OPENAI_API_KEY')
 GLM_BASE_URL = os.getenv('GLM_OPENAI_BASE_URL')
+MCP_SERVER_URL = os.getenv('MCP_SERVER_URL', 'http://127.0.0.1:8000/mcp')
+SKILLS_DIR = os.getenv('SKILLS_DIR', './skills')
+WORK_DIR = os.getenv('WORK_DIR', './workspace')
+SUBAGENTS_YAML = os.getenv('SUBAGENTS_YAML', './subagents.yaml')
 
+print(GLM_API_KEY, GLM_BASE_URL, MCP_SERVER_URL, SKILLS_DIR, WORK_DIR, SUBAGENTS_YAML)
 def create_glm_model():
 
     model = ChatOpenAI(
@@ -15,21 +25,80 @@ def create_glm_model():
         base_url=GLM_BASE_URL,
         temperature=0.7,
         max_tokens=20000,
+        streaming=True,
     )
     return model
 
+def create_mcp_client():
+    mcp_client = MultiServerMCPClient(
+            {
+                "weather": {
+                    "url": MCP_SERVER_URL,
+                    "transport": "http",
+                }
+            }
+        )
+    return mcp_client
+
+def load_subagents(config_path) -> list:
+    with open(config_path) as f:
+        config = yaml.safe_load(f)
+    available_tools = {}
+    subagents = []
+    for name, spec in config.items():
+        subagent = {
+            "name": name,
+            "description": spec["description"],
+            "system_prompt": spec["system_prompt"],
+        }
+        if "model" in spec:
+            subagent["model"] = spec["model"]
+        if "tools" in spec:
+            avt = []
+            for t in spec["tools"]:
+                if t in available_tools:
+                    avt.append(available_tools[t])
+
+            subagent["tools"] = avt
+        subagents.append(subagent)
+
+    return subagents
+
 class YardManager:
 
-    def __init__(self):
+    def __init__(self, config=None):
+        self.config = config
         pass
     
-    async def create_agent(self):
-        pass
+    @classmethod
+    async def create(cls, config=None):
+        yard_manager = cls(config)
+        mcp_client = create_mcp_client()
+        yard_manager.tools = await mcp_client.get_tools()
+        yard_manager.agent = agent = create_deep_agent(
+            model=create_glm_model(),
+            tools=yard_manager.tools, 
+            memory=["./AGENTS.md"],
+            skills=[SKILLS_DIR], 
+            subagents=load_subagents(SUBAGENTS_YAML),
+            backend=FilesystemBackend(root_dir=WORK_DIR),
+            checkpointer=MemorySaver(),  
+        )
+        return yard_manager
 
-    async def run_single_turn(self, user_input: str):
-        pass
-    
 
-    
-    async def achat(self, user_input: str):
-        pass
+    async def achat(self, user_input: str, thread_id = "yard-manager-demo"):
+        async for chunk in self.agent.astream(
+            {"messages": [("user", user_input)]},
+            config={"configurable": {"thread_id": thread_id}},
+            stream_mode="messages",
+        ):
+                print(chunk)
+
+
+if __name__ == "__main__":
+    async def main():
+        yard_manager = await YardManager.create()
+        await yard_manager.achat("Hello, how are you?")
+
+    asyncio.run(main())
