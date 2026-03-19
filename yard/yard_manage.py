6bc4bea7e00175d5613bd9e1a840c055d7905ff3
lpl <linpeilu@fairlandgroup.com>
2026-03-19 18:08:04 +0800

    update graph

--- yard/yard_manage.py
+++ yard/yard_manage.py
@@ -136,12 +136,12 @@ if __name__ == "__main__":
         res = ""
         yard_manager = await YardManager.create()
         # print graph
-        # graph = yard_manager.agent.get_graph(xray=True)
-        # graph.draw_mermaid_png(output_file="yard_manager.png")
-        async for chunk in yard_manager.achat("帮我割草"):
-            content = chunk.get("content", "")
-            res += content
-        print(res)
+        graph = yard_manager.agent.get_graph(xray=True)
+        graph.draw_mermaid_png(output_file_path="yard_manager.png")
+        # async for chunk in yard_manager.achat("帮我割草"):
+        #     content = chunk.get("content", "")
+        #     res += content
+        # print(res)
 
 
     asyncio.run(main())
